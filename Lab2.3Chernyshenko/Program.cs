e89135249bc433e0ec6a7874ecaa90885e0ec6d1
uliyalyaaas <uliyaliya.1304@gmail.com>
2023-11-13 15:58:59 +0200

    Добавьте файлы проекта.

---
+++ Lab2.3Chernyshenko/Program.cs
@@ -0,0 +1,36 @@
+﻿double F1 = 30, D1 = 121;
+double F2 = -15, D2 = 50;
+double F3 = 87, D3 = 223;
+
+Console.WriteLine("Первая точка:");
+if (F1 >= 0)
+    Console.Write("Северо-");
+else
+    Console.WriteLine("Южно-");
+
+if (D1 >= 0)
+    Console.WriteLine("Восточное полушарие");
+else
+    Console.WriteLine("Западное полушарие");
+
+Console.WriteLine("Вторая точка:");
+if (F2 >= 0)
+    Console.WriteLine("Северо-");
+else
+    Console.Write("Южно-");
+
+if (D2 >= 0)
+    Console.WriteLine("Восточное полушарие");
+else
+    Console.WriteLine("Западное полушарие");
+
+Console.WriteLine("Третья точка:");
+if (F3 >= 0)
+    Console.Write("Северо-");
+else
+    Console.WriteLine("Южно-");
+
+if (D3 >= 0)
+    Console.WriteLine("Восточное полушарие");
+else
+    Console.WriteLine("Западное полушарие");
